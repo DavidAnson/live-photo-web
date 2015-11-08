3b304a06c7bf217d64bcf754da4f352f58c62ce5
David Anson <david@dlaa.me>
2015-11-07 23:17:06 -0800

    Add support for touch events.

--- live-photo-image.js
+++ live-photo-image.js
@@ -2,19 +2,26 @@
 
 var LivePhotoImagePrototype = Object.create(HTMLImageElement.prototype);
 
+LivePhotoImagePrototype.start = function() {
+  if (this.src && !this.originalSrc) {
+    this.originalSrc = this.src;
+    this.src = this.src.replace(/\.[^\.]+$/, ".gif");
+  }
+};
+
+LivePhotoImagePrototype.stop = function() {
+  if (this.originalSrc) {
+    this.src = this.originalSrc;
+    this.originalSrc = null;
+  }
+};
+
 LivePhotoImagePrototype.createdCallback = function() {
-  this.addEventListener("mouseenter", function() {
-    if (this.src) {
-      this.originalSrc = this.src;
-      this.src = this.src.replace(/\.[^\.]+$/, ".gif");
-    }
-  });
-  this.addEventListener("mouseleave", function() {
-    if (this.originalSrc) {
-      this.src = this.originalSrc;
-      this.originalSrc = null;
-    }
-  });
+  this.addEventListener("mouseenter", this.start);
+  this.addEventListener("mouseleave", this.stop);
+  this.addEventListener("touchstart", this.start);
+  this.addEventListener("touchcancel", this.stop);
+  this.addEventListener("touchend", this.stop);
 };
 
 var LivePhotoImage = document.registerElement("live-photo", {
