6c5cba8aad000c7cfa84dd334bbd50ba63bf70b1
David Anson <david@dlaa.me>
2015-11-10 22:35:05 -0800

    Use event.preventDefault.

--- live-photo-image.js
+++ live-photo-image.js
@@ -2,17 +2,19 @@
 
 var LivePhotoImagePrototype = Object.create(HTMLImageElement.prototype);
 
-LivePhotoImagePrototype.start = function() {
+LivePhotoImagePrototype.start = function(evt) {
   if (this.src && !this.originalSrc) {
     this.originalSrc = this.src;
     this.src = this.src.replace(/\.[^\.]+$/, ".gif");
+    evt.preventDefault();
   }
 };
 
-LivePhotoImagePrototype.stop = function() {
+LivePhotoImagePrototype.stop = function(evt) {
   if (this.originalSrc) {
     this.src = this.originalSrc;
     this.originalSrc = null;
+    evt.preventDefault();
   }
 };
 
